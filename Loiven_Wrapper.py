7849974418ce264a38c9964ddca57b3bc8d6414f
cbongiorno <pvofeta@gmail.com>
2016-07-10 20:53:07 +0200

    bug

--- Loiven_Wrapper.py
+++ Loiven_Wrapper.py
@@ -45,59 +45,59 @@ def Lypou(graph,n,seed_rand,seed=None,vm=None,var=True,cmpOut=False):
 
 def LoivenOriginal(g,n,seed,part=None):
 	DIRs = 'Community_latest/'
-    to_ck = [e.tuple for e in g.es]
+	to_ck = [e.tuple for e in g.es]
 
-    Od= OrderedDict.fromkeys(list(flatten(to_ck)))
+	Od= OrderedDict.fromkeys(list(flatten(to_ck)))
 
-    Od = dict(zip(Od.keys(),range(len(Od))))
+	Od = dict(zip(Od.keys(),range(len(Od))))
 
-    x = [(Od[a],Od[b],w) for (a,b),w in zip(to_ck,g.es["weight"])]
+	x = [(Od[a],Od[b],w) for (a,b),w in zip(to_ck,g.es["weight"])]
 
-    x = pd.DataFrame(x)
+	x = pd.DataFrame(x)
 
-    x.to_csv('/tmp/graph_%d.txt'%n,sep=' ',index=False,header=False)
-
-    
-    os.system(DIRs+'convert  -i /tmp/graph_%d.txt -o /tmp/graph_%d.bin -w /tmp/graph_%d.weights'%(n,n,n))
-
-    if part!=None:
-        Part = np.zeros(g.vcount()).astype(int)
-        for i,p in enumerate(part):
-            Part[Od[i]] = p
-
-        Part = zip(range(g.vcount()),Part)
-
-        pd.DataFrame(Part).to_csv('/tmp/Part_%d.part'%n,sep=' ',header=False,index=False)
+	x.to_csv('/tmp/graph_%d.txt'%n,sep=' ',index=False,header=False)
 
 
-    if part!=None:
-        os.system(DIRs+'community %d /tmp/graph_%d.bin -l -1 -w /tmp/graph_%d.weights -p /tmp/Part_%d.part > /tmp/graph_%d.tree'%(seed,n,n,n,n))
-    else:
-        os.system(DIRs+'community %d /tmp/graph_%d.bin -l -1 -w /tmp/graph_%d.weights  > /tmp/graph_%d.tree'%(seed,n,n,n))
+	os.system(DIRs+'convert  -i /tmp/graph_%d.txt -o /tmp/graph_%d.bin -w /tmp/graph_%d.weights'%(n,n,n))
 
-    print DIRs+'hierarchy /tmp/graph_%d.tree'%n
-    lv = int(os.popen(DIRs+'hierarchy /tmp/graph_%d.tree'%n).read().split('\n')[0].split(': ')[1])-1
+	if part!=None:
+		Part = np.zeros(g.vcount()).astype(int)
+		for i,p in enumerate(part):
+			Part[Od[i]] = p
 
-    
-    os.system(DIRs+'hierarchy /tmp/graph_%d.tree -l %d > /tmp/graph_node2comm_level2_%d'%(n,lv,n))
+		Part = zip(range(g.vcount()),Part)
 
-    cf = pd.read_csv('/tmp/graph_node2comm_level2_%d'%n,header=None,sep=' ')[1]
-
-    Odrv = dict(map(list,map(reversed,Od.items())))
-
-    c = np.array([-1]*g.vcount())
-    if cf[0]!='of':
-        for i,a in enumerate(cf):
-            c[Odrv[i]] = a
-
-    iso = np.where(c==-1)[0]
-    c[iso] = range(max(c)+1,max(c)+len(iso)+1)
-
-    c = c.astype(int)
+		pd.DataFrame(Part).to_csv('/tmp/Part_%d.part'%n,sep=' ',header=False,index=False)
 
 
-    os.system('rm /tmp/graph_%d.bin /tmp/graph_%d.tree /tmp/graph_%d.weights /tmp/graph_node2comm_level2_%d'%(n,n,n,n))
-    if part!=None:
-        os.system('rm  /tmp/Part_%d.part'%n)
+	if part!=None:
+		os.system(DIRs+'community %d /tmp/graph_%d.bin -l -1 -w /tmp/graph_%d.weights -p /tmp/Part_%d.part > /tmp/graph_%d.tree'%(seed,n,n,n,n))
+	else:
+		os.system(DIRs+'community %d /tmp/graph_%d.bin -l -1 -w /tmp/graph_%d.weights  > /tmp/graph_%d.tree'%(seed,n,n,n))
 
-    return c
+	print DIRs+'hierarchy /tmp/graph_%d.tree'%n
+	lv = int(os.popen(DIRs+'hierarchy /tmp/graph_%d.tree'%n).read().split('\n')[0].split(': ')[1])-1
+
+
+	os.system(DIRs+'hierarchy /tmp/graph_%d.tree -l %d > /tmp/graph_node2comm_level2_%d'%(n,lv,n))
+
+	cf = pd.read_csv('/tmp/graph_node2comm_level2_%d'%n,header=None,sep=' ')[1]
+
+	Odrv = dict(map(list,map(reversed,Od.items())))
+
+	c = np.array([-1]*g.vcount())
+	if cf[0]!='of':
+		for i,a in enumerate(cf):
+			c[Odrv[i]] = a
+
+	iso = np.where(c==-1)[0]
+	c[iso] = range(max(c)+1,max(c)+len(iso)+1)
+
+	c = c.astype(int)
+
+
+	os.system('rm /tmp/graph_%d.bin /tmp/graph_%d.tree /tmp/graph_%d.weights /tmp/graph_node2comm_level2_%d'%(n,n,n,n))
+	if part!=None:
+		os.system('rm  /tmp/Part_%d.part'%n)
+
+	return c
